50b15977156b19c65271149a82fc68210d5ccbed
renersullivan1 <renersullivan1@email.com>
2023-11-07 17:37:03 -0300

    adicionando page settings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SettingsComponent } from './settings/settings/settings.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'setings', component:SettingsComponent },
+  {path: '', component: SettingsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
