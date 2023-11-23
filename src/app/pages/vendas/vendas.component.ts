67f97a32c30fbc5e071e632871319a34a1f23c21
renersullivan1 <renersullivan1@email.com>
2023-11-22 23:20:15 -0300

    criando page endas

--- src/app/pages/vendas/vendas.component.ts
+++ src/app/pages/vendas/vendas.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-vendas',
   templateUrl: './vendas.component.html',
   styleUrls: ['./vendas.component.css']
 })
-export class VendasComponent {
+export class VendasComponent implements OnInit{
+
+  results!: any;
+  metaData!:any;
+
+  page: number = 1;
+  itemPerPage: string = '10';
+  filter?: string = 'cidade'
+
+  constructor(private http: HttpClient) { }
+
+  city: string = '';
+
+  ngOnInit(): void {
+    this.requestPage()
+  }
+
+  public onChange(param: any) {
+   this.itemPerPage = param.value
+}
+
+  public requestPage(page?: number, itemPerPage?: string | null) {
+
+    this.page = page ||  this.page
+    this.itemPerPage = itemPerPage || this.itemPerPage 
+
+    if(this.city){
+      this.http.get<any>(`http://138.68.232.90:3333/vendas`).subscribe(
+        result => {
+          console.log(result);
+          this.results = result.data;
+          this.metaData = result.meta
+        },
+      )
+      return
+    }
+    this.http.get<any>(`http://138.68.232.90:3333/vendas`).subscribe(
+      result => {
+        console.log(result);
+        this.results = result.data;
+        this.metaData = result.meta
+      },
+    )
+  }
+
+  public resetPageAndSearch(){
+    this.page = 1
+    this.requestPage()
+  }
+
+  
 
 }
+
